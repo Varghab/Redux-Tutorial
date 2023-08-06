f34dbcb6f815737bb73fd78917467d3a067d6e64
Varghab <shibvarghab@gmail.com>
2023-08-06 14:40:36 +0530

    Documentation update

--- Redux/documentation.md
+++ Redux/documentation.md
@@ -1,4 +1,4 @@
-Understand the Terms - 
+#Understand the Terms - 
 
 1. Store - The store is the central and single source of truth for the application's state. It holds the entire state tree of your application. In Redux, there should only be one store in your application. The store is responsible for managing the state, handling actions, and notifying the view (usually a UI component) when the state changes.
 
@@ -15,3 +15,16 @@ The Redux flow can be summarized as follows:
 3. Reducers handle the dispatched actions and update the state.
 4. The store notifies subscribers (usually UI components) about the state changes.
 5. UI components re-render based on the updated state.
+
+
+* When we are dispatching any action it should be dispatched instantly, meaning that if we dispatch any data in the payload and the data is coming after awaiting any api call then the program will give us error. Why so? As said the action should be dispatched instantly and return a plain object. Refer to the code, the code will give error as in the payload we are getting the data.price after awaiting the api call.
+
+```
+async function initUser(val){
+    const {data} = await axios.get('https://fakestoreapi.com/products');
+    return ({type:init, payload:{amount:data.price}})
+}
+
+store.dispatch(initUser())
+
+```
--- Redux/index.js
+++ Redux/index.js
@@ -1,31 +1,52 @@
 import { createStore } from 'redux';
-
+import axios from 'axios';
+// Declaration of constants so that name of the action type doesn't change by mistake
+const init = "INIT";
 const inc = "INCREMENT";
 const dec = "DECREMENT";
 const incByAmt = "INCREMENTBYAMOUNT";
 
+//The initial global state for the store
 const initialState = {
     amount:1,
     val: 99
 }
 
+//Creating the store by passing the reduce function as argument
 const store = createStore(reducer);
 
 
 function reducer(state=initialState, action){
-    if(action.type === inc){
-        return {...state, amount: state.amount+1};
-    }
-    if(action.type === dec){
-        return {...state, amount: state.amount-1};
-    }
-    if(action.type === incByAmt){
-        return {...state, amount: state.amount + action.payload.amount};
-    }
-    return state;
+    switch(action.type){
+        case init:
+            return {...state, amount: action.payload.amount};
+        case inc:
+            return {...state, amount: state.amount+1};
+        case dec:
+            return {...state, amount: state.amount-1};
+        case incByAmt:
+            return {...state, amount: state.amount + action.payload.amount};
+
+        default:
+            return state;
+        }
+    
 }
 
+//Async API call
+
+async function getUser(){
+    const {data} = await axios.get('https://fakestoreapi.com/products');
+    console.log(data);
+}
+
+getUser();
+
 //Action creators - 
+async function initUser(val){
+    const {data} = await axios.get('https://fakestoreapi.com/products');
+    return ({type:init, payload:{amount:data.price}})
+}
 
 function increment(){
     return {type:inc};
@@ -43,7 +64,8 @@ function incrementByAmount(value){
 // store.dispatch({type:"decrement"})
 // store.dispatch({type:"incrementByAmount", payload:{amount:4}})
 // store.dispatch({type:"incrementByAmount", payload:{amount:4}})
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(incrementByAmount(7));
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(incrementByAmount(7));
+store.dispatch(initUser());
 console.log(store.getState());
